94bd8cb93d2bd183b927e72503cf4382300fc148
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-09-01 19:35:10 +0300

    1295. Find Numbers with Even Number of Digits: Runtime: 1 ms, faster than 97.23% of Java online submissions for Find Numbers with Even Number of Digits. Memory Usage: 39 MB, less than 84.56% of Java online submissions for Find Numbers with Even Number of Digits.

---
+++ com.leetcode.training/src/main/java/com/leetcode/training/easy/Q1295_FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,22 @@
+package com.leetcode.training.easy;
+
+/**
+ * https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+ * @author alper
+ *
+ */
+public class Q1295_FindNumbersWithEvenNumberOfDigits {
+
+    public int findNumbers(final int[] nums) {
+    	int evenCount = 0;
+    	
+    	for (int i = 0; i < nums.length; i++) {
+    		if(String.valueOf(nums[i]).length() %2 == 0) {
+    			evenCount++;
+    		}
+		}
+    	
+		return evenCount;
+    }
+    
+}
---
+++ com.leetcode.training/src/test/java/com/leetcode/training/easy/Q1295_FindNumbersWithEvenNumberOfDigitsTest.java
@@ -0,0 +1,41 @@
+package com.leetcode.training.easy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+ * @author alper
+ *
+ */
+class Q1295_FindNumbersWithEvenNumberOfDigitsTest {
+
+	Q1295_FindNumbersWithEvenNumberOfDigits question;
+	
+	@BeforeEach
+    void setUp() {
+    	question = new Q1295_FindNumbersWithEvenNumberOfDigits();
+    }
+
+	@ParameterizedTest
+	@MethodSource("provideArray")
+	void test(int[] input, int output) {
+		int returnOutput = question.findNumbers(input);
+		assertThat(returnOutput).isEqualTo(output);
+	}
+	
+	private static Stream<Arguments> provideArray() {
+	    return Stream.of(
+	      Arguments.of(new int[] {12,345,2,6,7896}, 2),
+	      Arguments.of(new int[] {555,901,482,1771}, 1),
+	      Arguments.of(new int[] {1,2,3}, 0),
+	      Arguments.of(new int[] {111111,20,3333}, 3)
+	    );
+	}
+}
