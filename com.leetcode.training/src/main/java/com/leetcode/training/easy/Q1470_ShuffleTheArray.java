dbcec6d4d6c84a0e781735eb9a592125aa304b48
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-09-01 16:33:39 +0300

    1470. Shuffle the Array solution

---
+++ com.leetcode.training/src/main/java/com/leetcode/training/easy/Q1470_ShuffleTheArray.java
@@ -0,0 +1,34 @@
+package com.leetcode.training.easy;
+
+/**
+ * https://leetcode.com/problems/shuffle-the-array/
+ * @author alper
+ *
+ */
+public class Q1470_ShuffleTheArray {
+
+    public int[] shuffle(final int[] nums, final int n) {
+    	//print(nums, "INPUT");
+    	final int output[] = new int[nums.length];
+    	int i;
+    	
+    	for (i = 0; i < n; i++) {
+			output[i * 2] = nums[i];
+		}
+    	for (i=n; i < nums.length; i++) {
+    		//System.out.println("INDEX:" + ((i%n * 2) +1) + " i:" + i);
+    		output[i%n * 2 +1] = nums[i];
+		}
+    	//print(output, "OUTPUT");
+    	return output;
+    }
+    
+    void print(int[] output, String text) {
+    	System.out.print(text +" ");
+    	for (int i : output) {
+    		System.out.print(i + ", ");
+		}
+    	System.out.println(text + " END");
+    }
+    
+}
---
+++ com.leetcode.training/src/test/java/com/leetcode/training/easy/Q1470_ShuffleTheArrayTest.java
@@ -0,0 +1,36 @@
+package com.leetcode.training.easy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class Q1470_ShuffleTheArrayTest {
+
+	Q1470_ShuffleTheArray question;
+	
+    @BeforeEach
+    void setUp() {
+    	question = new Q1470_ShuffleTheArray();
+    }
+	
+    @ParameterizedTest
+	@MethodSource("provideData")
+	public void testRemoveDuplicates(int[] nums, int n, int[] output) throws Exception {
+		int[] returnOutput = question.shuffle(nums, n);
+		assertThat(returnOutput).isEqualTo(output);
+	}
+    
+	private static Stream<Arguments> provideData() {
+	    return Stream.of(
+	      Arguments.of(new int[] {2,5,1,3,4,7}, 3, new int[] {2,3,5,4,1,7}),
+	      Arguments.of(new int[] {1,2,3,4,4,3,2,1}, 4, new int[] {1,4,2,3,3,2,4,1}),
+	      Arguments.of(new int[] {1,1,2,2}, 2, new int[] {1,2,1,2})
+	    );
+	}
+
+}
