3d3bd99a392a8ada9543536fadf36255f5f86e07
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-09-08 15:00:06 +0300

    Runtime: 0 ms, faster than 100.00% of Java online submissions for Matrix Diagonal Sum. Memory Usage: 39.3 MB, less than 93.92% of Java online submissions for Matrix Diagonal Sum.

---
+++ leetcode-training/src/main/java/com/leetcode/training/easy/Q1572_MatrixDiagonalSum.java
@@ -0,0 +1,40 @@
+package com.leetcode.training.easy;
+
+public class Q1572_MatrixDiagonalSum {
+
+	/**
+	 * https://leetcode.com/problems/matrix-diagonal-sum/
+	 *  Constraints:
+		n == mat.length == mat[i].length
+		1 <= n <= 100
+		1 <= mat[i][j] <= 100
+		so we can use short max:32767
+	 * @param mat
+	 * @return
+	 */
+    public int diagonalSum(final int[][] mat) {
+    	if( mat.length ==1) {
+    		return mat[0][0];
+    	}
+    	final short n = (short) (mat.length -1);
+    	short total = 0;
+    	
+    	for (int y = 0; y <= n; y++) {
+    		total +=  mat[y][y] + mat[n-y][y];  		
+		}
+    	if(n%2==0) {
+    		total -= mat[n/2][n/2];
+    	}
+        return total;
+    }
+    
+    /**
+    public static void testShort(String[] args) {
+		short total = 0;
+		for (int i = 0; i < 100; i++) {
+			total += 100 + 100;
+		}
+		System.out.println(total);
+	}
+	*/
+}
---
+++ leetcode-training/src/test/java/com/leetcode/training/easy/Q1572_MatrixDiagonalSumTest.java
@@ -0,0 +1,38 @@
+package com.leetcode.training.easy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class Q1572_MatrixDiagonalSumTest {
+	
+	Q1572_MatrixDiagonalSum question;
+	
+	@BeforeEach
+    void setUp() {
+    	question = new Q1572_MatrixDiagonalSum();
+    }
+
+	@ParameterizedTest
+	@MethodSource("provideArray")
+	public void testDiagonalSum(int[][] input, int output) throws Exception {
+		int returnOutput = question.diagonalSum(input);
+		assertThat(returnOutput).isEqualTo(output);
+	}
+	
+	private static Stream<Arguments> provideArray() {
+	    return Stream.of(	      
+	      Arguments.of(new int[][] {{1,2,3},{4,5,6}, {7,8,9}}, 25),
+	      Arguments.of(new int[][] {{1,1,1,1},{1,1,1,1}, {1,1,1,1}, {1,1,1,1}}, 8),
+	      Arguments.of(new int[][] {{5}}, 5),
+	      //extra:
+	      Arguments.of(new int[][] {{1,2},{4,5}}, 12)
+	    );
+	}
+
+}
