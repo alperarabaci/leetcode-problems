dcdc1999960aac16f9c321ce293cbbb947423b2a
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-09-08 16:28:52 +0300

    1475. Final Prices With a Special Discount in a Shop Runtime: 1 ms, faster than 99.48% of Java online submissions for Final Prices With a Special Discount in a Shop. Memory Usage: 39.2 MB, less than 99.24% of Java online submissions for Final Prices With a Special Discount in a Shop.

---
+++ leetcode-training/src/main/java/com/leetcode/training/easy/Q1475_FinalPricesWithASpecialDiscountinAShop.java
@@ -0,0 +1,32 @@
+package com.leetcode.training.easy;
+
+/**
+ * https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
+ * @author alper
+ *
+ */
+public class Q1475_FinalPricesWithASpecialDiscountinAShop {
+
+    public int[] finalPrices(final int[] prices) {
+    	short i = 0;
+    	short j = (short) (i+1);
+    	for (i = 0; i < prices.length-1; i++) {
+			for (j = (short) (i+1); j < prices.length; j++) {
+				if(prices[j] <= prices[i]) {
+					prices[i] -=  prices[j];
+					break;
+				}
+			}
+		}
+    	//print(prices, "");
+    	return prices;
+    }
+    
+    void print(int[] output, String text) {
+    	System.out.print(text +" ");
+    	for (int i : output) {
+    		System.out.print(i + ", ");
+		}
+    	System.out.println(text + " END");
+    }
+}
---
+++ leetcode-training/src/test/java/com/leetcode/training/easy/Q1475_FinalPricesWithASpecialDiscountinAShopTest.java
@@ -0,0 +1,37 @@
+package com.leetcode.training.easy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class Q1475_FinalPricesWithASpecialDiscountinAShopTest {
+
+	Q1475_FinalPricesWithASpecialDiscountinAShop question;
+	
+	@BeforeEach
+    void setUp() {
+    	question = new Q1475_FinalPricesWithASpecialDiscountinAShop();
+    }
+	
+	@ParameterizedTest
+	@MethodSource("provideArray")
+	public void testFinalPrices(int[] input, int[] output) throws Exception {
+		int[] returnOutput = question.finalPrices(input);
+		assertThat(returnOutput).isEqualTo(output);
+	}
+	
+	private static Stream<Arguments> provideArray() {
+	    return Stream.of(	      
+	      Arguments.of(new int[] {8,4,6,2,3}, new int[] {4,2,4,2,3}),
+	      Arguments.of(new int[] {1,2,3,4,5}, new int[] {1,2,3,4,5}),
+	      Arguments.of(new int[] {10,1,1,6}, new int[] {9,0,1,6})
+	    );
+	}
+}
