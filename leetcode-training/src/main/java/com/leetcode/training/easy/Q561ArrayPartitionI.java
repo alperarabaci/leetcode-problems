8c747d60c18a2e8c444d7c1898597234496b6cce
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-09-08 19:17:27 +0300

    Already ordered.

--- leetcode-training/src/main/java/com/leetcode/training/easy/Q561ArrayPartitionI.java
+++ leetcode-training/src/main/java/com/leetcode/training/easy/Q561ArrayPartitionI.java
@@ -7,8 +7,12 @@ public class Q561ArrayPartitionI {
     public int arrayPairSum(int[] nums) {
     	Arrays.sort( nums);
     	int total = 0;
+    	/**
+    	 * Already ordered :(
+    	 * No need any compare operation
+    	 */
     	for (int i = 0; i < nums.length/2; i++) {
-			total += (nums[i*2] < nums[(i*2)+1])? nums[i*2]:nums[(i*2)+1];
+			total += nums[i*2] ;
 		}
 		return total;
     }
